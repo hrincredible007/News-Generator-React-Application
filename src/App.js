5ecb771ec4a8fd4b8f402ed6b8082242afdb0de2
hrincredible007 <hritikbhardwaj2002@gmail.com>
2023-08-09 01:20:40 +0530

    News App Navbar and News Fetching is done through hardcoded json

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import NavBar from './components/NavBar'
+// import NavBar2 from './components/NavBar2'
+import News from './components/News'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+
+        <NavBar/>
+        <News/>
+        
+      </div>
+    )
+  }
+}
---
+++ src/components/NewsItem.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+export default class NewsItem extends Component {
+
+  sty = {
+    width : '18rem'
+  }
+  render() {
+    
+    let {title, description, url, imageUrl} = this.props;
+    return (
+      <div> 
+        <div className="card" style={this.sty}>
+          <img className="card-img-top" src={imageUrl} alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}.</h5>
+            <p className="card-text">
+              {description}
+            </p>
+            <a href= {url} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
