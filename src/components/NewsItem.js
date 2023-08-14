0a7392f99048283518234571c86104c9065296f6
hrincredible007 <hritikbhardwaj2002@gmail.com>
2023-08-14 19:10:31 +0530

    Loader added before fetching the news content.

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -10,7 +10,7 @@ export default class NewsItem extends Component {
     let {title, description, url, imageUrl} = this.props;
     return (
       <div> 
-        <div className="card" style={this.sty}>
+        <div className="card text-center" style={this.sty}>
           <img className="card-img-top" src={!imageUrl?"https://www.livemint.com/lm-img/img/2023/08/08/600x338/11r_5g_1676953434658_1691480169340.png":imageUrl} alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
